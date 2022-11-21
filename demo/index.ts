b6fb343a424101d2c69bb0afb66ea913005a047c
Simon Reinisch <simon@reinisch.io>
2022-11-21 17:09:11 +0100

    Fix broken video links in demo page
    Improve the way videos are loaded

--- demo/index.ts
+++ demo/index.ts
@@ -1,19 +1,39 @@
 import { createCinematicEffect, version } from '../src';
 import './index.css';
 
-const buttons = Array.from(document.querySelectorAll('.controls > button'));
-const video = document.querySelector('.video-container > .video') as HTMLVideoElement;
+const buttons = Array.from(document.querySelectorAll('.controls > button')) as HTMLButtonElement[];
+const target = '.video-container > .video-background';
 
-createCinematicEffect({
-  target: '.video-container > .video-background',
-  src: video,
-});
+let switching = false;
+let video = document.querySelector('.video-container > .video') as HTMLVideoElement;
+let cleanup = createCinematicEffect({ target, src: video });
+
+const loadVideo = async (button: HTMLButtonElement) => {
+  if (switching) return;
+  buttons.forEach((btn) => btn.setAttribute('disabled', 'true'));
+
+  const newVideo = document.createElement('video');
+  newVideo.classList.add('video');
+  newVideo.autoplay = true;
+  newVideo.muted = true;
+
+  newVideo.addEventListener('canplay', () => {
+    buttons.forEach((btn) => btn.removeAttribute('disabled'));
+    buttons.forEach((btn) => btn.classList[btn === button ? 'add' : 'remove']('active'));
+
+    cleanup();
+    cleanup = createCinematicEffect({ target, src: newVideo });
+
+    switching = false;
+    video.replaceWith(newVideo);
+    video = newVideo;
+  });
+
+  newVideo.src = `./videos/${button.getAttribute('name')}.mp4`;
+};
 
 for (const button of buttons) {
-  button.addEventListener('click', () => {
-    video.src = `/videos/${button.getAttribute('name')}.mp4`;
-    buttons.forEach((btn) => btn.classList[btn === button ? 'add' : 'remove']('active'));
-  });
+  button.addEventListener('click', () => void loadVideo(button));
 }
 
 /* eslint-disable no-console */
